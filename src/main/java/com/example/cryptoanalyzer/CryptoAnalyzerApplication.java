c586602f1dbeee365cd535b95015f5dc7669cc6c
Sergey.Ablaev <mailto:sergei.ablaev@soft-control.ru>
2026-01-10 16:22:00 +0400

    EnableAsync

--- src/main/java/com/example/cryptoanalyzer/CryptoAnalyzerApplication.java
+++ src/main/java/com/example/cryptoanalyzer/CryptoAnalyzerApplication.java
@@ -3,8 +3,10 @@ package com.example.cryptoanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CryptoAnalyzerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CryptoAnalyzerApplication.class, args);
--- src/main/java/com/example/cryptoanalyzer/market/service/MarketDataService.java
+++ src/main/java/com/example/cryptoanalyzer/market/service/MarketDataService.java
@@ -2,24 +2,42 @@
 package com.example.cryptoanalyzer.market.service;
 
 import com.example.cryptoanalyzer.market.ws.BinanceWebSocketClient;
+import com.example.cryptoanalyzer.market.ws.TradeEvent;
 import com.example.cryptoanalyzer.ohlc.service.OhlcAggregator;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class MarketDataService {
 
-    public MarketDataService(BinanceWebSocketClient client, OhlcAggregator aggregator) {
-        client.subscribe(trade -> {
-            aggregator.onTrade(trade)
-                      .ifPresent(c -> log.debug("Closed candles: {}", c));
-        });
+    private final BinanceWebSocketClient client;
+    private final OhlcAggregator aggregator;
+    private final ApplicationContext applicationContext;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void subscribeToMarketData() {
+        MarketDataService proxy = applicationContext.getBean(MarketDataService.class);
+        client.subscribe(proxy::processTrade);
+        log.info("Subscribed to market data via context proxy");
     }
 
-    @PostConstruct
-    public void init() {
-        // triggers WS client creation
+    @Async
+    public void processTrade(TradeEvent trade) {
+//        log.debug("Current thread: {}", Thread.currentThread().getName());
+        aggregator.onTrade(trade)
+                  .ifPresent(c -> log.debug("Closed candles: {}", c));
     }
+
+//    @PostConstruct
+//    public void init() {
+//        // triggers WS client creation
+//    }
 }

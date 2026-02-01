640b3097c94775a1e7c51481ed4e9ecd0f3b2fa9
Sergey.Ablaev <ablaev.sergey@gmail.com>
2026-02-01 18:28:57 +0400

    Added System.exit on H2 DB critical error, GlobalErrorHandler  refactoring.

--- src/main/java/com/example/cryptoanalyzer/market/service/MarketDataService.java
+++ src/main/java/com/example/cryptoanalyzer/market/service/MarketDataService.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
+import org.springframework.dao.DataAccessException;
+//import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -31,9 +33,28 @@ public class MarketDataService {
 
     @Async
     public void processTrade(TradeEvent trade) {
-//        log.debug("Current thread: {}", Thread.currentThread().getName());
-        aggregator.onTrade(trade)
-                  .ifPresent(c -> log.debug("Closed candles: {}", c));
+
+        try {
+        aggregator.onTrade(trade).ifPresent(c -> log.debug("Closed candles: {}", c));
+        } catch (DataAccessException ex) {
+            log.error("CRITICAL DB ERROR (Table NOT FOUND). Initiating an APPLICATION RESTART!...", ex);
+            stopSystem();
+        }
+    }
+
+    /**
+     * Starting shutdown in a new thread: Giving time for the logs to be written.
+     */
+    private static void stopSystem() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {}
+
+            // Simple exit (It works well in case of restart policy having in Docker/K8s)
+            System.exit(1);
+
+        }).start();
     }
 
 //    @PostConstruct
--- src/main/java/com/example/cryptoanalyzer/web/AlertController.java
+++ src/main/java/com/example/cryptoanalyzer/web/AlertController.java
@@ -3,19 +3,17 @@ package com.example.cryptoanalyzer.web;
 import com.example.cryptoanalyzer.alerts.model.AlertEvent;
 import com.example.cryptoanalyzer.alerts.service.AlertEventService;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/alerts")
+@RequiredArgsConstructor
 public class AlertController {
 
     private final AlertEventService service;
-
-    public AlertController(AlertEventService service) {
-        this.service = service;
-    }
 
     @GetMapping
     public List<AlertEvent> list(
